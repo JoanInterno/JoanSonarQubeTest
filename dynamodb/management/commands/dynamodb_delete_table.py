02bdcfa289d5169ce1c4bc6fd94a96b6707ab838
Joan Mari <jmyern90@gmail.com>
2024-10-30 19:54:10 +0100

    change credentials to environ

--- dynamodb/management/commands/dynamodb_delete_table.py
+++ dynamodb/management/commands/dynamodb_delete_table.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError, CommandParser
 from dynamodb.utils.DynamoDBInterface import Movies
 import boto3
+import os
 
 
 class Command(BaseCommand):
@@ -8,7 +9,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         public_key = "AKIAQ4NSA5YQSGK77YXI"
-        secret_key = input(f"Password for key {public_key}: ")
+        secret_key = os.environ["AWS_PRIVATE_KEY"]
         session = boto3.Session(
             aws_access_key_id=public_key,
             aws_secret_access_key=secret_key,
